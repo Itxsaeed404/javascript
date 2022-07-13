ad6dc7a81dce129984cd3180392da178681130ef
Saeed Shah <57037552+Itxsaeed404@users.noreply.github.com>
2022-07-13 22:03:04 +0500

    Add files via upload

---
+++ Prescription Form using HTML - CSS -Javascript/main.js
@@ -0,0 +1,67 @@
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+const searchBox = document.querySelector(".search-box input");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
+
+  searchBox.value = "";
+  filterList("");
+
+  if (optionsContainer.classList.contains("active")) {
+    searchBox.focus();
+  }
+});
+
+optionsList.forEach(o => {
+  o.addEventListener("click", () => {
+    selected.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
+  });
+});
+
+searchBox.addEventListener("keyup", function(e) {
+  filterList(e.target.value);
+});
+
+const filterList = searchTerm => {
+  searchTerm = searchTerm.toLowerCase();
+  optionsList.forEach(option => {
+    let label = option.firstElementChild.nextElementSibling.innerText.toLowerCase();
+    if (label.indexOf(searchTerm) != -1) {
+      option.style.display = "block";
+    } else {
+      option.style.display = "none";
+    }
+  });
+};
+
+
+
+function myFunction1() {
+  var x = document.getElementById("section1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction2() {
+  var x = document.getElementById("section2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction3() {
+  var x = document.getElementById("section3");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
